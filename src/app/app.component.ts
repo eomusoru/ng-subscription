407b21bab51e527560a2f279c6816ff82f719fde
Eduard <eduardomusoru@gmail.com>
2017-07-07 16:47:14 +0300

    Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent{
+  @ViewChild('f') formObj: NgForm;
+
+  subscriptions = ['Basic', 'Advanced', 'Pro'];
+  selectedSubscription = 'Advanced';
+  submitedObject = false;
+
+  subscriptionObj = {
+    email: '',
+    subscription: '',
+    password: ''
+  }
+
+  onSubmit(){
+    this.subscriptionObj.email = this.formObj.value.email;
+    this.subscriptionObj.subscription = this.formObj.value.subscription;
+    this.subscriptionObj.password = this.formObj.value.password;
+    this.formObj.reset();
+    this.selectedSubscription = 'Advanced';
+    this.submitedObject = true;
+  }
 }
